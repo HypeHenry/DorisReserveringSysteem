eeb5caa9d09c19c887870ce3b8ef9a98ef94684c
Henry Ofusu Barima <H.Barima@Me.com>
2018-01-19 12:01:32 +0100

    html twig

--- Dorisproject/src/AppointmentBundle/Controller/DefaultController.php
+++ Dorisproject/src/AppointmentBundle/Controller/DefaultController.php
@@ -14,4 +14,44 @@ class DefaultController extends Controller
     {
         return $this->render('AppointmentBundle:Default:index.html.twig');
     }
+
+    /**
+     * @Route("/hairproducts")
+     */
+    public function hairProductsAction()
+    {
+        return $this->render('AppointmentBundle:Default:hairProducts.html.twig');
+    }
+
+    /**
+     * @Route("/hairstyles")
+     */
+    public function hairStylesAction()
+    {
+        return $this->render('AppointmentBundle:Default:hairStyles.html.twig');
+    }
+
+    /**
+     * @Route("/hairstyles/appointment")
+     */
+    public function portfolioAction()
+    {
+        return $this->render('AppointmentBundle:Default:Portfolio.html.twig');
+    }
+
+    /**
+     * @Route("/contact")
+     */
+    public function contactAction()
+    {
+        return $this->render('AppointmentBundle:Default:contact.html.twig');
+    }
+
+    /**
+     * @Route("/about")
+     */
+    public function aboutMeAction()
+    {
+        return $this->render('AppointmentBundle:Default:aboutMe.html.twig');
+    }
 }
